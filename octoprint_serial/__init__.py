e8424b3d027c08aa1afa89e07ebe149b1b5b4134
treefrog65 <johnson.trenten@gmail.com>
2021-12-09 12:48:51 -0600

    Update __init__.py

--- octoprint_serial/__init__.py
+++ octoprint_serial/__init__.py
@@ -1,1 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
 
+import octoprint.plugin
+import serial
+
+ser = serial.Serial('/dev/ttyACM0',115200)
+
+class ArmPlugin(octoprint.plugin.StartupPlugin,octoprint.plugin.TemplatePlugin):
+    def on_after_startup(self):
+        self._logger.info("Controls 6DOF Arm Movement.")
+
+
+__plugin_name__ = "Arm Control"
+__plugin_version__ = "1.0.0"
+__plugin_description__ = "Control Arduino based Robotic arm"
+__plugin_pythoncompat__ = ">=2.7,<4"
+__plugin_implementation__ = ArmControlPlugin()
